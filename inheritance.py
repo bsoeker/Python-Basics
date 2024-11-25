1b02b00b039912fa808cc6e1454b1d7ef9c98a71
Barlas Söker <bsoker98@gmail.com>
2024-11-25 23:22:54 +0300

    inheritance basic example

---
+++ inheritance.py
@@ -0,0 +1,17 @@
+class Animal:
+    def eat(self):
+        print("eating!?")
+
+
+class Mammal(Animal):
+    def walk(self):
+        print("Glamorous walking!")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("Swimming...")
+
+
+horse = Mammal()
+horse.eat()
